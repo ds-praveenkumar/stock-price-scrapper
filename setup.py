57e083b40a6b86d9d8e8dd86d3ecc0204a9cb3e7
ds-praveenkumar <praveenkr2208@gmail.com>
2020-06-11 01:34:32 +0530

    stocks module added

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 # Created by ds-praveenkumar at 09-06-2020 22 42
 # feature:
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name = 'personal-project-env',
+    name='personal-project-env',
     version='0.1.0',
     licence='MIT',
     description='project for personal interest',
-    packages=find_packages('bulls-run.src','bulls-run'),
+    packages=find_packages('bulls-run.src', 'bulls-run'),
     install_requires=[
         'requests',
         'beautifulsoup4',
